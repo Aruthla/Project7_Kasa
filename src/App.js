335c5a8be138d4c1c5485eee230880f1f3adb584
Laëtitia Cliquennois <laetitia.cli@live.fr>
2023-03-03 15:52:23 +0100

    Correction

--- src/App.js
+++ src/App.js
@@ -2,32 +2,21 @@ import Home from './pages/home/Home';
 import Error404 from './pages/error404/Error404';
 import About from './pages/about/About';
 import Accomodation from './pages/accomodation/Accomodation';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-
-const router = createBrowserRouter([
-	{
-		path: "/Kasa/",
-		element: <Home />
-	},
-	{
-		path: "/accomodation/:id",
-		element: <Accomodation />
-	},
-	{
-		path: '/about',
-		element: <About />
-	},
-	{
-		path: "*",
-		element: <Error404 />
-	},
-]);
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from "react";
 
 function App() {
 	return (
-		<>
-			<RouterProvider router={router}/>
-		</>
+		<React.StrictMode>
+		<Router>
+			<Routes>
+				<Route path="/" element={<Home />}/>
+				<Route path="/accomodation/:id" element={<Accomodation />}/>
+				<Route path="/about" element={<About />}/>
+				<Route path="*" element={<Error404 />}/>
+			</Routes>
+        </Router>
+    </React.StrictMode>
 	);
 }
 
